7969d7886d8f623146c5d3e2b8a40ac701c8b24a
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-20 02:21:18 -0400

    feat: config endpoint put

--- Frontend/src/components/InputFile/InputFile.tsx
+++ Frontend/src/components/InputFile/InputFile.tsx
@@ -1,3 +1,23 @@
+import { useState } from 'react';
+import { uploadImage, PUT_IMAGE } from './../../helpers'
+
 export const InputFile = () => {
-  return <input type="file" name="" id="" />;
+  const [state, setState] = useState<any>(null);
+
+  const handleFile = (e) => {
+    let file: any = e.target.files[0];
+    setState({ file });
+  }
+
+  const handleUpload = async (e) => {
+    console.log(state.file);
+    await uploadImage(state.file, PUT_IMAGE);
+  }
+  
+  return (
+    <>
+    <input type="file" name="file" id="" onChange={e => handleFile(e)} />
+    <button onClick={e => handleUpload(e)}>button</button>
+    </>
+  )
 };
---
+++ Frontend/src/helpers/index.ts
@@ -0,0 +1,2 @@
+export { uploadImage } from './api/config'
+export { PUT_IMAGE } from './api/url'
