7d3e3c02c605c52bd89c7eeb737a4689c02b5d29
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-20 09:46:34 -0400

    feat(backend): add service

---
+++ Backend/services/picasso_service.py
@@ -0,0 +1,11 @@
+# Python
+from typing import Annotated
+
+class PicassoService:
+    def __init__(self, file: bytes) -> None:
+        self.file = file
+        self.author: str | None = "The author"
+        self.type_picture: str | None = "The Picture Type"
+
+    def __str__(self) -> str:
+        return f"Author is: {self.author} and your type pincture is {self.type_picture}"
