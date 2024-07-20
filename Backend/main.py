80d0f511590f7b831b43e257ec767c32d6e30a57
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-20 13:44:46 -0400

    fix(backnend): allow origin with cors

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,15 @@
 # FastAPI
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # Network
 from networks.picasso_network import picasso
 
 app = FastAPI()
+
+origins = [
+    '*'
+]
 
 
 @app.get("/")
@@ -14,3 +19,12 @@ async def root():
 
 # Routers FastAPI
 app.include_router(picasso, prefix='/api/v1')
+
+# Middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
