7c2f051aa231986a7406ab190f277ac77cb0af37
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-20 13:25:03 -0400

    feat(frontend): add backend helper

---
+++ Frontend/src/helpers/backend_helper.ts
@@ -0,0 +1,14 @@
+import { APIClient } from "./api_helper";
+import { Endpoints } from "./url_helper";
+
+const api = new APIClient()
+
+// Upload Image Method
+export const uploadImage = (file: any) => {
+    const config = {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        }
+    }
+    return api.put(Endpoints.uploadImage, file, config)
+}
